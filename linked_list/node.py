35a3e3637c81d6824647fe5f4223245a217928c7
kheng2yee <kheng2yee@gmail.com>
2019-02-23 06:14:39 +0000

    <hellowwwe>

--- linked_list/node.py
+++ linked_list/node.py
@@ -4,13 +4,22 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
+        if self.next:
+            return "Node({}) > Node({})",format(self.elem,self.next.elem)
+        else:
+            return "Node({}) >/",format(self.elem)
 
-    def __eq__(self, other):
-        pass
+# node = Node(9)
+# node.next = Node('X')
+# print(str(node))
 
-    def __repr__(self):
-        pass
+def __eq__(self,other):
+    return self.elem != other.elem
+
+# node1 = Node(9)
+# node2 = Node('X')
+# node3 = Node("test")
